e39f89f30599d5b8bc92813383aac430165cf5f9
DishaVerma <ms.dishaverma.20@gmail.com>
2026-03-16 15:06:05 +0530

    day 6

--- day6/index.js
+++ day6/index.js
@@ -1,32 +1,39 @@
-import http from 'http';
-
-const port = 8000;
-
+import http from "http";
+import { userLogin } from "../day_06/controller/login.js";
+const PORT = 8800;
 const server = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") return;
 
-    if (req.url === '/') {
-        res.end("Home Page");
+    if (req.url === "/login" && req.method === "POST") {
+        let body = "";
+        req.on("data", (dataChunk) => {
+            body += dataChunk.toString();
+        })
+
+        req.on("end", async () => {
+            const userData = JSON.parse(body);
+            const response = await userLogin(userData, "./user.json");
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(JSON.stringify(response));
+        })
+    } else {
+        res.writeHead(500, { "content-type": "application/json" });
+        res.end(JSON.stringify({ error: "url is not matched" }));
     }
 
-    else if (req.url === '/login') {
-        let body = '';
-
-        req.on('data', (chunk) => {
-            body += chunk.toString();
-        });
-
-        req.on('end', () => {
-            console.log(body);
-            res.end('Login successful');
-        });
-    }
-
-    else {
-        res.end("Page not found");
-    }
-
-});
-
-server.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+    // switch(req.url){
+    //     case "/":
+    //         res.end("Home Page");
+    //         break;
+    //     case "/login":
+    //         res.end("Login Page");
+    //         break;
+    //     case "/register":
+    //         res.end("register page");
+    //         break;
+    //     default:
+    //         res.end("Incorrect path");
+    // }
+})
+server.listen(PORT,
+    () => console.log(`server is running at http://localhost:${PORT}`))
